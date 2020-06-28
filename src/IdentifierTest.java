62bf0a0f974ba39d5eb59f78b78a15b5b682b458
gangeshsingh72 <singh.gangesh@gmail.com>
2020-06-28 20:55:09 +0100

    Identifier

---
+++ src/IdentifierTest.java
@@ -0,0 +1,15 @@
+public class IdentifierTest {
+    public static void main(String[] args) {
+        int 123a;
+        int _12a;
+        int abc;
+        int ABC;
+        int _123456a;
+        int void;
+        int String;
+        int do;
+        int Do;
+        int d0;
+
+    }
+}
